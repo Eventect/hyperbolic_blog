599ed57ee1a83b9ecbf58adc30cfbc7abcd9a16f
Paolo Tessarolo <p.tessarolo@gmail.com>
2012-10-10 20:32:15 +0200

    adds redirect to root if adding more than 1 admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,15 @@ class UsersController < Clearance::UsersController
   def new
     if User.all.length < 1
       super
+    else
+      redirect_to root_url
     end
   end
 
   def create
     if User.all.length < 1
       super
+      redirect_to root_url
     end
   end
 
